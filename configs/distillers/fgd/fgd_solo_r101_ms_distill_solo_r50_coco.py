7cdd34e8d59d94dd21c3cac5e2398d9464151515
yzd <yangzhendong.meso@bytedance.com>
2022-03-03 20:10:19 +0800

    add solo dis

---
+++ configs/distillers/fgd/fgd_solo_r101_ms_distill_solo_r50_coco.py
@@ -0,0 +1,72 @@
+_base_ = [
+    '../../solo/solo_r50_fpn_1x_coco.py'
+]
+# model settings
+find_unused_parameters=True
+alpha_fgd=0.00002
+lambda_fgd=0.65
+decouple=False
+distiller = dict(
+    type='DetectionDistiller',
+    teacher_pretrained = 'Solo_r101_3x.pth',
+    init_student = True,
+    distill_cfg = [ 
+                    dict(student_module = 'neck.fpn_convs.3.conv',
+                         teacher_module = 'neck.fpn_convs.3.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_fgd_fpn_3',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_fgd=alpha_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       decouple=decouple,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.2.conv',
+                         teacher_module = 'neck.fpn_convs.2.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_fgd_fpn_2',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_fgd=alpha_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       decouple=decouple,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.1.conv',
+                         teacher_module = 'neck.fpn_convs.1.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_fgd_fpn_1',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_fgd=alpha_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       decouple=decouple,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.0.conv',
+                         teacher_module = 'neck.fpn_convs.0.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_fgd_fpn_0',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_fgd=alpha_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       decouple=decouple,
+                                       )
+                                ]
+                        ),
+
+                   ]
+    )
+
+student_cfg = 'configs/solo/solo_r50_fpn_1x_coco.py'
+teacher_cfg = 'configs/solo/solo_r101_fpn_3x_coco.py'
+optimizer_config = dict(_delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
---
+++ configs/solo/solo_r101_fpn_3x_coco.py
@@ -0,0 +1,9 @@
+_base_ = './solo_r50_fpn_3x_coco.py'
+
+# model settings
+model = dict(
+    backbone=dict(
+        depth=101,
+        init_cfg=dict(type='Pretrained',
+                      checkpoint='torchvision://resnet101')))
+
